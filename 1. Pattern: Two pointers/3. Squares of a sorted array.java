1e19d4fd52672888935e6826604ab9c45670dbff
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2023-04-09 20:40:00 -0700

    Create 3. Squares of a sorted array.java

---
+++ 1. Pattern: Two pointers/3. Squares of a sorted array.java
@@ -0,0 +1,35 @@
+/*
+Problem Statement
+Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+Example 1:
+Input: [-2, -1, 0, 2, 3]
+Output: [0, 1, 4, 4, 9]
+*/
+/*
+Time complexity O(n)
+Space complexity O(n)
+*/
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int l=0, r=n-1;
+        int i=n-1;
+        while(l<=r)
+        {
+            int num1 = nums[l]*nums[l];
+            int num2 = nums[r]*nums[r];
+            if(num1>num2)
+            {
+                ans[i--] = num1;
+                l++;
+            }
+            else
+            {
+                ans[i--] = num2;
+                r--;
+            }
+        }
+        return ans;
+    }
+}
